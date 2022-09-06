38c48e0429f8dc51801527571b3deaad677525bc
Adarshgamer live <112461919+Adarsh-verma-ID@users.noreply.github.com>
2022-09-06 10:16:28 +0530

    Add files via upload

--- suggestions.js
+++ suggestions.js
@@ -1,13 +1,3 @@
 let suggestions = [
-"Adarshgamerlive",
-"google",
-"Craftgamee",
-"Github",
-"Netlify",
-"Youtube",
-"Cricket",
-"Cricket live", 
-"Cricket live score",
-"hackerrank",
-"hacker wallpaper"
+"Adarshgamerlive"
 ];
